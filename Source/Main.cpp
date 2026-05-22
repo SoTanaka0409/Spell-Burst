fc3cf43671a03d86cccaa0d7cef0f71d8e77ec6a
SoTanaka0409 <tky2502039@stu.o-hara.ac.jp>
2026-05-22 12:30:31 +0900

    y

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,82 +1,52 @@
 #include "DxLib.h"
-#include"Utility.h"
-#include <EffekseerForDXLib.h>
-// プログラムは WinMain から始まります
-/*============================================================================================================================================
-ビジュアルスタジオ2022から2026、2026から2022に変更する時のエラーは[プロジェクト->プロパティ->プラットホームツールセットをバージョンに合わせる]
-=============================================================================================================================================*/
+#include "Scene.h"
+#include "SceneManager.h"
+#include "Master.h"
+#include <iostream>
+#include "Utility.h"
 
 
+//SceneManager* Master::sceneManager = new SceneManager();
 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Truncate log file
+    FILE* fpLog = nullptr;
+    fopen_s(&fpLog, "debug.log", "w");
+    if (fpLog) fclose(fpLog);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{// ウインドウモードで起動
-	ChangeWindowMode(true);
+    // Start debug console
+    AllocConsole();
+    FILE* fp = nullptr;
+    freopen_s(&fp, "CONOUT$", "w", stdout);
+    freopen_s(&fp, "CONOUT$", "w", stderr);
+    printf("Debug Console Started!\n");
 
-	SetGraphMode(Utility::SCREEN_WIDTH, Utility::SCREEN_HEIGHT, 32, 60);
-	SetWindowSize(Utility::SCREEN_WIDTH, Utility::SCREEN_HEIGHT);
-	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
-	{
-		return -1;			// エラーが起きたら直ちに終了
-	}
+    // Init DX Library
+    ChangeWindowMode(TRUE);
+    SetGraphMode(Utility::SCREEN_WIDTH, Utility::SCREEN_HEIGHT, 32);
+    SetMainWindowText("Shooting Action Game");
 
-	SetGlobalAmbientLight(GetColorF(0.4f, 0.4f, 0.4f, 1.0f));
-	//描画先設定を裏画面に設定する
-	SetDrawScreen(DX_SCREEN_BACK);
+    if (DxLib_Init() == -1) {
+        return -1;
+    }
 
-	//Zバッファに書き込む準備
-	SetUseZBufferFlag(true);
-	SetWriteZBufferFlag(true);
+    SetDrawScreen(DX_SCREEN_BACK);
 
-	
+    Master::sceneManager->Initialize();
 
+    // Main loop
+    while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0) {
+        ClearDrawScreen();
 
-	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
-	{
+        Master::sceneManager->Update();
+        Master::sceneManager->Draw();
 
-		//画面を初期化する
-		ClearDrawScreen();
-		int time = GetNowCount();
+        ScreenFlip();
+    }
 
+    delete Master::sceneManager;
 
-		
-		// 画面全体を少し明るくする
-		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 50);
+    DxLib_End();
 
-		// 白い四角を画面全体に描画
-		//
-		DrawBox(0, 0, Utility::SCREEN_WIDTH, Utility::SCREEN_HEIGHT, GetColor(255, 255, 255), TRUE);
-
-		// ブレンドを戻す
-		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-		//Master::mpScoreManager->Draw();
-
-
-
-
-		//player->Draw();
-
-
-		//裏画面の内容を表画面に映すwwwwwwwwwwwwww
-		ScreenFlip();
-
-		//１７ミリ秒（秒数間訳６０フレームだったっ場合１フレーム当たりの経過時間
-		//経過するまでここで待つ
-		while (GetNowCount() - time < 17)
-		{
-			//待つだけなのでここには何も書かない
-		}
-
-		
-
-	}
-	//終了処理
-	
-
-	Effkseer_End();
-
-
-	DxLib_End();				// ＤＸライブラリ使用の終了処理
-
-	return 0;				// ソフトの終了 
+    return 0;
 }
