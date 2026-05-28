c602e5191fd11a42766dc5b9263beb75be0900b3
SoTanaka0409 <tky2502039@stu.o-hara.ac.jp>
2026-05-28 14:52:02 +0900

    Upgrade ResultScene with animations, background, particles, and styled text

--- Source/ResultScene.h
+++ Source/ResultScene.h
@@ -1,5 +1,16 @@
 #pragma once
 #include "Scene.h"
+#include <vector>
+
+struct ResultParticle {
+    float x, y;
+    float vx, vy;
+    float size;
+    float angle;
+    float rotSpeed;
+    int color;
+    int life;
+};
 
 class ResultScene : public Scene {
 public:
@@ -9,4 +20,13 @@ public:
     void Update() override;
     void Draw() override;
     void Finalize() override;
+
+private:
+    std::vector<ResultParticle> m_particles;
+    int m_stateTimer;
+    int m_bgGraph;
+    int m_playerGraph;
+    
+    // 縁取り付きテキスト描画用のヘルパー
+    void DrawOutlinedString(int x, int y, const char* str, unsigned int color, unsigned int outlineColor, int fontHandle);
 };
