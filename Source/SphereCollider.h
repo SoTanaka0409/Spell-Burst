ae08ae9b85cbfc6e235a32cd2a71481e82cba593
SoTanaka0409 <tky2502039@stu.o-hara.ac.jp>
2026-06-09 15:22:12 +0900

    l

---
+++ Source/SphereCollider.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Collider.h"
+
+class SphereCollider : public Collider
+{
+public:
+	SphereCollider(Object2D* parent, VECTOR center, float radius);
+	virtual ~SphereCollider();
+
+	void Update(Collider* check) override;
+	void Draw() override;
+
+	void OnEnter() override;
+	void OnTrigger() override;
+	void OnExit() override;
+
+};
