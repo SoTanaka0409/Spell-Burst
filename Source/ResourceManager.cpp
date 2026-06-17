36414bf55d0123a3385d2c3a44b4f4075c818448
SoTanaka0409 <tky2502039@stu.o-hara.ac.jp>
2026-06-17 22:42:27 +0900

    debugon

--- Source/ResourceManager.cpp
+++ Source/ResourceManager.cpp
@@ -1,4 +1,4 @@
-#include "ResourceManager.h"
+﻿#include "ResourceManager.h"
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
@@ -27,9 +27,25 @@ int ResourceManager::GetGraph(const std::string& path) {
     return handle;
 }
 
+int ResourceManager::GetFont(int size, int thickness) {
+    auto key = std::make_pair(size, thickness);
+    auto it = m_fontMap.find(key);
+    if (it != m_fontMap.end()) {
+        return it->second;
+    }
+
+    int handle = CreateFontToHandle(NULL, size, thickness, DX_FONTTYPE_ANTIALIASING);
+    m_fontMap[key] = handle;
+    return handle;
+}
+
 void ResourceManager::ClearAll() {
     for (auto& pair : m_graphMap) {
         DeleteGraph(pair.second);
     }
     m_graphMap.clear();
+    for (auto& pair : m_fontMap) {
+        DeleteFontToHandle(pair.second);
+    }
+    m_fontMap.clear();
 }
--- Source/ResourceManager.h
+++ Source/ResourceManager.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <map>
 #include <string>
 
@@ -6,16 +6,15 @@ class ResourceManager
 {
 private:
     std::map<std::string, int> m_graphMap;
-
-   
+    std::map<std::pair<int, int>, int> m_fontMap;
 
 public:
-
     ResourceManager();
     ~ResourceManager();
     static ResourceManager* GetInstance();
 
     int GetGraph(const std::string& path);
+    int GetFont(int size, int thickness);
 
     void ClearAll();
 };
